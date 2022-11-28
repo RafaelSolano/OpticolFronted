0f376294a3da2d0077cb9821ac9bd3a1895e91c3
Johan Vasquez <johanjvc7@gmail.com>
2022-11-28 18:23:15 -0500

    Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { MenuComponentComponent } from './components/menu-component/menu-compone
 import { BloqueHeroComponentComponent } from './components/bloque-hero-component/bloque-hero-component.component';
 
 import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { ServicesComponentComponent } from './components/services-component/services-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponentComponent,
     BloqueHeroComponentComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    ServicesComponentComponent
   ],
   imports: [
     BrowserModule,
